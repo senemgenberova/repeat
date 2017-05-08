c81a1a60595a38bc63e2c2e198d1ee563a3d34a9
User <User>
2017-05-08 18:43:08 +0400

    cs commit

---
+++ polindrom/polindrom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int temp_i = 0;
+            int reqem = 0;
+            int ters_i = 0;
+
+            for (int i = n; i <= m; i++)
+            {
+                temp_i = i;
+                while (temp_i != 0)
+                {
+                    reqem = temp_i % 10;
+                    ters_i = ters_i * 10 + reqem;
+                    temp_i /= 10;
+                }
+                if (ters_i == i)
+                {
+                    Console.WriteLine(i);
+                }
+                ters_i = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sequential_numbers/sequential_numbers/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sequential_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //List<int> myList = new List<int>();
+
+            //while(n != 0)
+            //{
+            //    myList.Add(n % 10);
+            //    n = n / 10;
+            //}
+
+            //int[] digits = myList.ToArray();
+            //int length = digits.Length;
+
+            //if (length % 2 == 1)
+            //{
+            //    if (digits[0] + digits[length-1] == 2 * digits[length / 2])
+            //    {
+            //        Console.WriteLine("duzdu");
+            //    }
+            //}
+
+            //else
+            //{
+            //    for (int i = 0; i < digits.Length; i++)
+            //    {
+            //        if (digits[i] + digits[length-1] == digits[i + 1] + digits[length - 2])
+            //        {
+            //            Console.WriteLine("duzdu");
+            //        }
+            //    }
+            //}
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int temp_i = 0;
+            List<int> myList = new List<int>();
+
+            for (int i = n; i <= m; i++)
+            {
+                while (temp_i != 0)
+                {
+                    myList.Add(temp_i % 10);
+                    temp_i /= 10;
+                }
+
+                int[] digits = myList.ToArray();
+                int length = digits.Length;
+
+                if (length % 2 == 1)
+                {
+                    if (digits[0] + digits[length - 1] == 2 * digits[length / 2])
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                else
+                {
+                    for (int j = 0; j < digits.Length; j++)
+                    {
+                        if (digits[j] + digits[length - 1] == digits[j + 1] + digits[length - 2])
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
